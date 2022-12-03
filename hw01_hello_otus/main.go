0a4ac268337ba0f4d698c38045779913f294eab9
skolzkyi <skolzkyi@gmail.com>
2022-12-03 17:32:20 +0400

    разработка завершена

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	string := "Hello, OTUS!"
+	reversedString := reverseString(string)
+	fmt.Println(reversedString)
+}
+
+func reverseString(input string) string {
+	output := stringutil.Reverse(input)
+	return output
 }

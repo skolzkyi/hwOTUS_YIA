380e0605f336e737860bccee26ee57caf0b2dfea
skolzkyi <skolzkyi@gmail.com>
2022-12-10 22:49:23 +0400

    task with asterisk completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,15 +2,17 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(input string) (string, error) {
 	var res string
+
+	void, _ := utf8.DecodeRune([]byte(``))
 
 	if input == "" {
 		return "", nil
@@ -24,35 +26,66 @@ func Unpack(input string) (string, error) {
 	}
 
 	for i := 0; i <= len(rslice)-2; i++ {
-		if unicode.IsDigit(rslice[i+1]) {
+		if isDigit(rslice, i+1) {
 			digit, _ := strconv.Atoi(string(rslice[i+1]))
 			for j := 0; j < digit; j++ {
 				resslice = append(resslice, rslice[i])
 			}
 			i++
-		} else {
+		} else if !isSlash(rslice, i) {
 			resslice = append(resslice, rslice[i])
+			rslice[i] = void
 		}
 	}
 
-	if !unicode.IsDigit(rslice[len(rslice)-1]) {
+	if !isDigit(rslice, len(rslice)-1) {
 		resslice = append(resslice, rslice[len(rslice)-1])
 	}
 
-	fmt.Println()
 	res = string(resslice)
 
 	return res, nil
 }
 
+// не исп. case т.к. условия нельзя свести к одному выражению и по идее
+// надо бы возвращать особую ошибку на каждый случай(в тестах можно только одну эту)
+
+//nolint:gocritic
 func verifRslice(input []rune) error {
 	for i := 0; i <= len(input)-1; i++ {
 		if i == 0 && unicode.IsDigit(input[i]) {
 			return ErrInvalidString
-		} else if i > 0 && unicode.IsDigit(input[i]) && unicode.IsDigit(input[i-1]) {
+		} else if i > 0 && isDigit(input, i) && isDigit(input, i-1) {
+			return ErrInvalidString
+		} else if i > 0 && unicode.IsLetter(input[i]) && isSlash(input, i-1) {
 			return ErrInvalidString
 		}
 	}
 
 	return nil
+}
+
+func isDigit(input []rune, index int) bool {
+	var res bool
+	if unicode.IsDigit(input[index]) && !isSlash(input, index-1) {
+		res = true
+	}
+
+	return res
+}
+
+func isSlash(input []rune, index int) bool {
+	var res bool
+	slash, _ := utf8.DecodeRune([]byte(`\`))
+	if index > 0 {
+		if input[index] == slash && input[index-1] != slash {
+			res = true
+		}
+	} else {
+		if input[index] == slash {
+			res = true
+		}
+	}
+
+	return res
 }

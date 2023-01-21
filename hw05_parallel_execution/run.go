8e3d9c969c33400dea7def22f6a26dd89f071ef4
skolzkyi <skolzkyi@gmail.com>
2023-01-21 18:32:36 +0400

    WIP 2

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,70 +2,112 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var ErrErrorsBadErrorsCount = errors.New("errors count less tasks len")
+var ErrErrorsBadGoroutinesCount = errors.New("tasks len less goroutines count")
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 
-	workch := make(chan Task, len(tasks))
-	errch := make(chan error)
-	signalch := make(chan struct{})
-	wg := sync.WaitGroup{}
+	var returnerror error
 
-	for i := 0; i <= len(tasks); i++ {
-		workch <- tasks[i]
+	if len(tasks) < n {
+		returnerror = ErrErrorsBadGoroutinesCount
+		return returnerror
 	}
-	close(workch)
 
-	wg.Add(n + 1)
+	if m > len(tasks) {
+		returnerror = ErrErrorsBadErrorsCount
+		return returnerror
+	}
 
-	go watchdog(n, workch, errch, signalch, wg)(n, workch, errch, signalch, wg)
-	<-signalch
+	workch := make(chan Task, n)
+	errch := make(chan error)
+
+	//var workerr error
+	wg := sync.WaitGroup{}
+	wgStop := sync.WaitGroup{}
+	wg.Add(n)
+	wgStop.Add(n)
+	var stopint int32
 
 	for i := 0; i < n; i++ {
-		go worker(tasks, workch, errch, wg)(tasks, workch, errch, wg)
+		workch <- tasks[i]
 	}
 
+	fmt.Println("count of GR: ", n, "buf: ", len(workch))
+
+	k := 0
+	for i := 0; i < n; i++ {
+		k++
+		go worker(i, workch, errch, &wg, &wgStop, &stopint)
+
+	}
+	fmt.Println("real count of GR: ", k)
+	k = 0
+	q := 0
+	t := n
+	var flag bool
+	for {
+		workerr := <-errch
+		if workerr != nil {
+			if !flag {
+				k++
+				fmt.Println("numerr: ", k)
+			} else {
+				q++
+				fmt.Println("numerr after stop: ", q)
+			}
+		}
+		if !flag {
+			if ((k < m || workerr == nil) || (m <= 0)) && (t <= len(tasks)-1) { //if ((k < m || workerr == nil) && (t <= len(tasks)-1)) || (m <= 0)
+				workch <- tasks[t]
+				fmt.Println("len: ", t)
+				t++
+			} else {
+				close(workch)
+				if (k >= m) && (m > 0) {
+					returnerror = ErrErrorsLimitExceeded
+				}
+				flag = true
+			}
+		}
+		fmt.Println("stopint: ", stopint)
+		if stopint >= int32(n) {
+			fmt.Println("break")
+			break
+		}
+	}
+	fmt.Println("freedom")
 	wg.Wait()
 
-	return nil
+	return returnerror
 }
 
-func worker(tasks []Task, workch chan Task, errch chan error, wg *sync.WaitGroup) {
+func worker(i int, workch chan Task, errch chan error, wg *sync.WaitGroup, wgStop *sync.WaitGroup, stopint *int32) {
+	//j := 1
+	defer wg.Done()
+	//fmt.Println("start worker: ", i)
 	for {
 		task, ok := <-workch
-		err := task()
-		if err != nil {
+		if !ok {
+			//fmt.Println("break worker: ", i)
+			atomic.AddInt32(stopint, int32(1))
+			//wgStop.Done()
+			break
+		} else {
+			//fmt.Println("worker: ", i, " task: ", j, "buf: ", len(workch))
+			//j++
+			err := task()
 			errch <- err
 		}
-		if !ok {
-			break
-		}
-		wg.Done()
 	}
-
-}
-
-func watchdog(n int, workch chan Task, errch chan error, signalch chan struct{}, wg *sync.WaitGroup) {
-
-	signalch <- struct{}{}
-
-	i := 0
-	for i < n {
-		<-errch
-		i++
-	}
-	for {
-		_, ok := <-workch
-		if !ok {
-			break
-		}
-
-	}
-	wg.Done()
+	//fmt.Println("stop worker: ", i)
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -16,7 +16,7 @@ func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
-		tasksCount := 50
+		tasksCount := 10
 		tasks := make([]Task, 0, tasksCount)
 
 		var runTasksCount int32
@@ -30,16 +30,53 @@ func TestRun(t *testing.T) {
 			})
 		}
 
-		workersCount := 10
-		maxErrorsCount := 23
+		workersCount := 4
+		maxErrorsCount := 2
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
+	})
+	//TODO
+	t.Run("errors count less tasks len", func(t *testing.T) {
+		tasksCount := 10
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		workersCount := 4
+		maxErrorsCount := 2
 		err := Run(tasks, workersCount, maxErrorsCount)
 
 		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
+	t.Run("if M <=0  - ignore error", func(t *testing.T) {
+		tasksCount := 10
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 4
+		maxErrorsCount := -1
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+		//require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.LessOrEqual(t, runTasksCount, int32(workersCount), "extra tasks were started")
+	})
+
 	t.Run("tasks without errors", func(t *testing.T) {
-		tasksCount := 50
+		tasksCount := 10
 		tasks := make([]Task, 0, tasksCount)
 
 		var runTasksCount int32

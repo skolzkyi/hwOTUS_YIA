46551998ed30b5d830131b80dae8a5f33939ed89
skolzkyi <skolzkyi@gmail.com>
2023-02-11 13:57:41 +0400

    work_in_progress

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +13,58 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+
+	var writedBytes int64
+
+	fileFrom, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	fmt.Println("check filefrom")
+	defer fileFrom.Close()
+
+	fileTo, err := os.Create(toPath)
+	fmt.Println("check fileto1")
+	if err != nil {
+		return err
+	}
+	fmt.Println("check fileto2")
+	defer fileTo.Close()
+
+	buffer := make([]byte, 1024)
+	fileFrom.Seek(offset, io.SeekStart)
+
+	var flag bool
+	for {
+		n, errFF := fileFrom.Read(buffer)
+		fmt.Println(n)
+		if n < len(buffer) {
+			buffer = buffer[:n]
+		}
+		//n, errFF := io.ReadFull(fileFrom, buffer)
+		writedBytes = writedBytes + int64(n)
+		if writedBytes >= limit && limit > 0 {
+			fmt.Println("writedbytes, owerwrite: ", writedBytes, writedBytes-limit)
+			buffer = buffer[:int64(len(buffer))-(writedBytes-limit)]
+			fmt.Println("new buffer length: ", len(buffer))
+			flag = true
+		}
+		_, errFT := fileTo.Write(buffer)
+		if errFT != nil {
+			return errFT
+		}
+		if errFF != nil {
+			if errors.Is(errFF, io.EOF) { //|| errors.Is(errFF, io.ErrUnexpectedEOF)      && n == 0
+				fmt.Println("exit: ", errFF.Error())
+				flag = true
+			} else {
+				return errFF
+			}
+		}
+		if flag {
+			break
+		}
+	}
+
 	return nil
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/skolzkyi/hwOTUS_YIA/hw07_file_copying
 
-go 1.16
+go 1.19

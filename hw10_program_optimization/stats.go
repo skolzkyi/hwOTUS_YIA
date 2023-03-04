78398df722ca1c078e24b996998d06d96aba21d4
skolzkyi <skolzkyi@gmail.com>
2023-03-04 19:51:25 +0400

    Optimization complete

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,99 +1,39 @@
 package hw10programoptimization
 
 import (
-	//"encoding/json"
 	"bufio"
-	"fmt"
 	"io"
-	"regexp"
 	"strings"
 
 	"github.com/tidwall/gjson"
 )
 
-type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
-}
-
-// cd C:\REPO\Go\!OTUS\hwOTUS_YIA\hw10_program_optimization
-
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	emails, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(emails, domain)
-}
 
-// type users []User
-type emails map[int64]string
-
-//type emails []string
-
-//type void struct{}
-
-func getUsers(r io.Reader) (*emails, error) {
-	/*
-		content, err := ioutil.ReadAll(r)
-		if err != nil {
-			return nil, err
-		}
-	*/
-	result := make(emails)
+	result := make(DomainStat)
 	bufReader := bufio.NewReader(r)
-	var i int64
 	var email string
 	var flag bool
+	var matched bool
+	var err error
+	var num int
+	var domainstr string
+	var line string
+	searchDomain := "." + domain
 	for {
-		line, err := bufReader.ReadString('\n')
+		line, err = bufReader.ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
-				//break
 				flag = true
 			} else {
 				return nil, err
 			}
 		}
 		email = gjson.Get(line, "Email").String()
-		result[i] = email
-		i++
-		if flag {
-			break
-		}
-	}
-	//fmt.Println("result: ", result)
-	//lines := strings.Split(string(content), "\n")
-	//length := len(lines)
-	//result := make(emails, length, length)
-	/*
-		var email string
-		for i, line := range lines {
-			email = gjson.Get(line, "Email").String()
-			result[i] = email
-		}
-	*/
-	return &result, nil
-}
 
-func countDomains(emails *emails, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-	var matched bool
-	var err error
-	var num int
-	var domainstr string
-	for _, email := range *emails {
-		matched, err = regexp.Match("\\."+domain, []byte(email))
-		if err != nil {
-			return nil, err
-		}
+		matched = strings.Contains(email, searchDomain)
 
 		if matched {
 			domainstr = strings.ToLower(strings.SplitN(email, "@", 2)[1])
@@ -101,6 +41,10 @@ func countDomains(emails *emails, domain string) (DomainStat, error) {
 			num++
 			result[domainstr] = num
 		}
+		if flag {
+			break
+		}
 	}
+
 	return result, nil
 }
--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -5,10 +5,18 @@ package hw10programoptimization
 
 import (
 	"bytes"
-	"fmt"
+	//"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+)
+
+const (
+	mb          uint64 = 1 << 20
+	memoryLimit uint64 = 30 * mb
+
+	timeLimit = 300 * time.Millisecond
 )
 
 func TestGetDomainStat(t *testing.T) {
@@ -38,8 +46,25 @@ func TestGetDomainStat(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, DomainStat{}, result)
 	})
+	t.Run("AdditionalBenchmark", func(t *testing.T) {
+		bench := func(b *testing.B) {
+			for i := 1; i < b.N; i++ {
+				res, _ := GetDomainStat(bytes.NewBufferString(data), "com")
+				b.Helper()
+				b.StopTimer()
+				//fmt.Println(result)
+				require.NotEqual(t, res, DomainStat{})
+				b.StartTimer()
+			}
+		}
+		result := testing.Benchmark(bench)
+		mem := result.MemBytes
+		t.Logf("time used: %s / %s", result.T, timeLimit)
+		t.Logf("memory used: %dMb / %dMb", mem/mb, memoryLimit/mb)
+	})
 }
 
+/*
 func AdditionalBenchmark(b *testing.B) {
 
 	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
@@ -54,3 +79,26 @@ func AdditionalBenchmark(b *testing.B) {
 		}
 	})
 }
+
+func AdditionalBenchmark2(t *testing.T) {
+	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+	bench := func(b *testing.B) {
+		for i := 1; i < b.N; i++ {
+			_, _ = GetDomainStat(bytes.NewBufferString(data), "com")
+			b.Helper()
+			b.StopTimer()
+			//fmt.Println(result)
+			b.StartTimer()
+		}
+	}
+	result := testing.Benchmark(bench)
+	mem := result.MemBytes
+	t.Logf("time used: %s / %s", result.T, timeLimit)
+	t.Logf("memory used: %dMb / %dMb", mem/mb, memoryLimit/mb)
+}
+*/
